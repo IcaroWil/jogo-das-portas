e102f6167c556faa5f6b84108c0f9faccc729db2
IcaroWil <icarofaria11@gmail.com>
2024-05-27 23:02:26 -0300

    Jogo-das-Portas

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,22 +1,39 @@
 import styles from '../styles/Porta.module.css'
+import Presente from './Presente'
 import PortalModel from '../model/porta'
 
 interface PortaProps {
     value: PortalModel
+    onChange: (novaPorta: PortalModel) => void
 }
 
 export default function Porta(props: PortaProps) {
     const  porta  = props.value
-    const selecionada = porta.selecionada ? styles.selecionada : ''
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
 
+    const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+    const abrir = e => {
+        e.stopPropagation()
+        props.onChange(porta.abrir())
+    }
+
+    function renderizarPorta() {
+        return (
+            <div className={styles.porta}>
+                <div className={styles.numero}>{porta.numero}</div>
+                <div className={styles.macaneta}
+                    onClick={abrir}></div>
+            </div>
+        )
+    }
 
     return (
-        <div className={styles.area}>
+        <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta.numero}</div>
-                    <div className={styles.macaneta}></div>
-                </div>
+                {porta.fechada ? 
+                    renderizarPorta() : 
+                    porta.temPresente ? <Presente/> : false 
+                }
             </div>
             <div className={styles.chao}></div>
         </div>
